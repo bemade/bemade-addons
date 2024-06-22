cd03903ea4cad52e00b7ff40caf2c3a2b2e4578c
Marc Durepos <marc@bemade.org>
2024-06-22 09:43:47 -0400

    caldav_sync: Added some error handling for almost-correct calendar URLs.

--- caldav_sync/__manifest__.py
+++ caldav_sync/__manifest__.py
@@ -7,28 +7,28 @@
 #    For details, visit https://www.gnu.org/licenses/lgpl-3.0.en.html
 
 {
-    'name': 'CalDAV Synchronization',
-    'version': '17.0.0.5.3',
-    'license': 'LGPL-3',
-    'category': 'Productivity',
-    'summary': 'Synchronize Odoo Calendar Events with CalDAV Servers',
-    'description': """
+    "name": "CalDAV Synchronization",
+    "version": "17.0.0.5.4",
+    "license": "LGPL-3",
+    "category": "Productivity",
+    "summary": "Synchronize Odoo Calendar Events with CalDAV Servers",
+    "description": """
         This module allows Odoo to synchronize calendar events with CalDAV servers.
         Users can synchronize their Odoo calendar with external applications like
         Apple Calendar or Thunderbird using CalDAV protocol.
     """,
-    'author': 'Bemade Inc.',
-    'website': 'https://www.bemade.org',
-    'depends': ['base', 'calendar'],
-    'external_dependencies': {
-        'python': ['caldav', 'icalendar', 'bs4'],
+    "author": "Bemade Inc.",
+    "website": "https://www.bemade.org",
+    "depends": ["base", "calendar"],
+    "external_dependencies": {
+        "python": ["caldav", "icalendar", "bs4"],
     },
-    'images': ['static/description/images/main_screenshot.png'],
-    'data': [
-        'views/res_users_views.xml',
-        'data/caldav_sync_data.xml',
+    "images": ["static/description/images/main_screenshot.png"],
+    "data": [
+        "views/res_users_views.xml",
+        "data/caldav_sync_data.xml",
     ],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
+    "installable": True,
+    "application": True,
+    "auto_install": False,
 }
