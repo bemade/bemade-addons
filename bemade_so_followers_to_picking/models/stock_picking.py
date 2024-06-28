afb4d84e8f4ccf67be10d6541a89b314d29e6bb8
Marc Durepos <marc@bemade.org>
2024-06-28 13:25:19 -0400

    [FIX] bemade_so_followers_to_picking: re-indent method to fix module.

--- bemade_so_followers_to_picking/models/stock_picking.py
+++ bemade_so_followers_to_picking/models/stock_picking.py
@@ -2,18 +2,19 @@ from odoo import models, api
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        pickings = super().create(vals_list)
 
-@api.model_create_multi
-def create(self, vals_list):
-    pickings = super().create(vals_list)
+        for picking, vals in zip(pickings, vals_list):
+            if "origin" in vals:
+                sale_order = self.env["sale.order"].search(
+                    [("name", "=", vals["origin"])], limit=1
+                )
+                if sale_order:
+                    for follower in sale_order.message_follower_ids:
+                        picking.message_subscribe(partner_ids=[follower.partner_id.id])
 
-    for picking, vals in zip(pickings, vals_list):
-        if 'origin' in vals:
-            sale_order = self.env['sale.order'].search([('name', '=', vals['origin'])], limit=1)
-            if sale_order:
-                for follower in sale_order.message_follower_ids:
-                    picking.message_subscribe(partner_ids=[follower.partner_id.id])
-
-    return pickings
+        return pickings
