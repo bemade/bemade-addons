aed5ad9aedd5483fb5f3d45a0591436e023c7937
Marc Durepos <marc@bemade.org>
2024-07-03 16:56:00 -0400

    impersonate_user: new module

---
+++ impersonate_user/__manifest__.py
@@ -0,0 +1,35 @@
+#
+#    Bemade Inc.
+#
+#    Copyright (C) 2023-June Bemade Inc. (<https://www.bemade.org>).
+#    Author: Marc Durepos (Contact : marc@bemade.org)
+#
+#    This program is under the terms of the GNU Lesser General Public License,
+#    version 3.
+#
+#    For full license details, see https://www.gnu.org/licenses/lgpl-3.0.en.html.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+#    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#    DEALINGS IN THE SOFTWARE.
+#
+{
+    "name": "Impersonate User",
+    "version": "17.0.0.0.1",
+    "summary": "Allow administrators to impersonate other users",
+    "category": "Administration",
+    "author": "Bemade Inc.",
+    "website": "http://www.bemade.org",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/impersonation_views.xml",
+    ],
+    "assets": {},
+    "installable": True,
+    "auto_install": False,
+}
