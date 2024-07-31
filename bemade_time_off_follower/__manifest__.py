5a5354869370e12b2c73d18d79204afa3f296755
Marc Durepos <marc@bemade.org>
2024-07-31 15:28:40 -0400

    time_off_follower: fix a bug due to badly named method override + add test

--- bemade_time_off_follower/__manifest__.py
+++ bemade_time_off_follower/__manifest__.py
@@ -1,22 +1,15 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Time Off Alternative Follower",
-    "version": "17.0.0.0.1",
+    "version": "17.0.0.0.2",
     "category": "Extra Tools",
-    'summary': 'Add Alternative Follower When Receiving Message While On Time Off',
-    "description": """
-        Add Alternative Follower When Receiving Message While On Time Off
-    """,
+    "summary": "Add Alternative Follower When Receiving Message While On Time Off",
     "author": "Bemade",
-    'website': 'https://www.bemade.org',
-    "depends": [
-        'hr_holidays',
-        'mail'
-    ],
+    "website": "https://www.bemade.org",
+    "depends": ["hr_holidays", "mail"],
     "data": [
-        'views/hr_leave_views.xml',
+        "views/hr_leave_views.xml",
     ],
     "auto_install": False,
     "installable": True,
-    'license': 'OPL-1'
+    "license": "LGPL-3",
 }
