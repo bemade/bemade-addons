77cd7eba403c8cf970300122d280c1785e989b5a
Marc Durepos <marc@bemade.org>
2024-10-25 10:55:34 -0400

    get rid of purpose migration in fsm_equipment to fix migration bug

--- fsm_equipment/migrations/17.0.0.2.0/post-migrate.py
+++ fsm_equipment/migrations/17.0.0.2.0/post-migrate.py
@@ -15,22 +15,12 @@ def migrate(cr, version):
     purposes = cr.dictfetchall()
 
     env = api.Environment(cr, SUPERUSER_ID, {})
-    tags = env["fsm.equipment.tag"].create(
-        [{"name": purpose["name"]} for purpose in purposes]
-    )
 
-    purpose_dict = {
-        purpose["id"]: tags.filtered(lambda tag: tag.name == purpose["name"]).id
-        for purpose in purposes
-    }
-
-    _logger.info(f"purpose_dict: {purpose_dict}")
     env["fsm.equipment"].create(
         [
             {
                 "name": component["name"],
                 "sequence": component["sequence"],
-                "tag_ids": [Command.link(purpose_dict[component["purpose_id"]])],
                 "parent_id": component["equipment_id"],
                 "description": component["note"],
                 "product_id": component["product_id"],
