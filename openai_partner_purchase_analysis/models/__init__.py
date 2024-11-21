fdcc81d29e4696b74f4018279e0195db2f537b70
xtremxpert <benoit@vezina.biz>
2024-11-21 09:56:19 -0500

    openai fix

--- openai_partner_purchase_analysis/models/__init__.py
+++ openai_partner_purchase_analysis/models/__init__.py
@@ -1,4 +1,4 @@
 from . import res_partner
-from . import partner_purchase_analysis_wizard
+# from . import partner_purchase_analysis_wizard
 from . import res_config_settings
 from . import res_company
