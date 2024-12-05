f34860bfacba1590bcffc244ede8958eca9b652b
Marc Durepos <marc@bemade.org>
2024-12-05 16:56:28 -0500

    mig to 18.0 for bemade's modules

--- bemade_pwa_config/__manifest__.py
+++ bemade_pwa_config/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Bemade PWA Configuration',
-    'version': '1.0',
+    'version': '18.0.0.1.0',
     'summary': 'Manage PWA settings including dynamic app icons',
     'description': """
 Manage Progressive Web App Settings
