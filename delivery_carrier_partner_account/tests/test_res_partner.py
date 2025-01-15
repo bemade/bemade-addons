2a1be1885e9f76ea6686cd8b384d45a2350cdd3a
Marc Durepos <marc@bemade.org>
2025-01-15 10:13:52 -0500

    delivery_carrier_partner_account: added some test cases and fixed some bugs relating default accounts

--- delivery_carrier_partner_account/tests/test_res_partner.py
+++ delivery_carrier_partner_account/tests/test_res_partner.py
@@ -28,3 +28,72 @@ class TestResPartner(TransactionCase):
         self.assertEqual(
             partner.carrier_account_ids[0], partner.default_carrier_account_id
         )
+
+    def test_default_carrier_set_on_update(self):
+        partner = self.env["res.partner"].create(
+            {
+                "name": "Test Partner",
+            }
+        )
+        partner.write(
+            {
+                "carrier_account_ids": [
+                    Command.create(
+                        {
+                            "delivery_carrier_id": self.env.ref(
+                                "delivery.free_delivery_carrier"
+                            ).id,
+                            "account_number": "1234567890",
+                        }
+                    )
+                ]
+            }
+        )
+        self.assertEqual(
+            partner.carrier_account_ids[0], partner.default_carrier_account_id
+        )
+
+    def test_no_change_to_default_account_id_on_update_if_already_set(self):
+        partner = self.env["res.partner"].create(
+            {
+                "name": "Test Partner",
+                "carrier_account_ids": [
+                    Command.create(
+                        {
+                            "delivery_carrier_id": self.env.ref(
+                                "delivery.free_delivery_carrier"
+                            ).id,
+                            "account_number": "1234567890",
+                        }
+                    )
+                ],
+            }
+        )
+        new_account = self.env["delivery.carrier.account"].create(
+            {
+                "partner_id": partner.id,
+                "delivery_carrier_id": self.env.ref(
+                    "delivery.delivery_local_delivery"
+                ).id,
+                "account_number": "1234567890",
+            }
+        )
+        self.assertNotEqual(partner.default_carrier_account_id, new_account)
+
+    def test_carrier_set_if_account_created_from_other_side(self):
+        partner = self.env["res.partner"].create(
+            {
+                "name": "Test Partner",
+            }
+        )
+        new_account = self.env["delivery.carrier.account"].create(
+            {
+                "partner_id": partner.id,
+                "delivery_carrier_id": self.env.ref(
+                    "delivery.free_delivery_carrier"
+                ).id,
+                "account_number": "1234567890",
+            }
+        )
+
+        self.assertEqual(partner.default_carrier_account_id, new_account)
