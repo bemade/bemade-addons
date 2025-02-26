31892a892a76d91779f0b976e8c2805018debbc2
Marc Durepos <marc@bemade.org>
2025-02-26 12:14:15 -0500

    attempt fix to delivery carrier account - selecting collect account raising error

--- delivery_carrier_partner_account/wizard/choose_delivery_carrier.py
+++ delivery_carrier_partner_account/wizard/choose_delivery_carrier.py
@@ -15,9 +15,25 @@ class ChooseDeliveryCarrier(models.TransientModel):
         if self.delivery_billing_mode:
             vals.update(delivery_billing_mode=self.delivery_billing_mode)
         if self.carrier_account_id:
-            vals.update(carrier_account_id=self.carrier_account_id)
+            vals.update(carrier_account_id=self.carrier_account_id.id)
         if self.carrier_id:
             vals.update(carrier_id=self.carrier_id.id)
-        self.order_id.with_context(no_carrier_update=True).write(vals)
+
+        # Ensure we have a valid carrier account
+        if (
+            self.carrier_id
+            and self.delivery_billing_mode
+            and not self.carrier_account_id
+        ):
+            # Force recompute of valid carrier accounts
+            self._compute_valid_carrier_account_ids()
+            default_account = self._get_default_carrier_account()
+            if default_account:
+                vals.update(carrier_account_id=default_account.id)
+
+        # Write values to the order before calling super
+        if vals:
+            self.order_id.with_context(no_carrier_update=True).write(vals)
+
         res = super().button_confirm()
         return res
