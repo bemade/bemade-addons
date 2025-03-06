ec757e1883e5ef20f6d1f41752f0eafa04d2f488
xtremxpert <benoit@vezina.biz>
2025-03-06 13:01:41 -0500

    test passed need double check

--- replace_invoicing_user/models/res_config_settings.py
+++ replace_invoicing_user/models/res_config_settings.py
@@ -11,6 +11,13 @@ class ResConfigSettings(models.TransientModel):
         help='If checked, the current user will be set as the invoice user. Otherwise, a specific user will be used.'
     )
 
+    current_user_as_follower = fields.Boolean(
+        string='Add Current User as Follower',
+        config_parameter='current_user_as_invoice_user.current_user_as_follower',
+        default=False,
+        help='If checked, the current user will be added as a follower to the invoice when using current user as invoice user.'
+    )
+
     specific_invoice_user_id = fields.Many2one(
         comodel_name='res.users',
         string='Specific Invoice User',
@@ -24,8 +31,10 @@ class ResConfigSettings(models.TransientModel):
         res = super(ResConfigSettings, self).get_values()
         ICP = self.env['ir.config_parameter'].sudo()
         use_current_user = ICP.get_param('current_user_as_invoice_user.use_current_user', 'True')
+        current_user_as_follower = ICP.get_param('current_user_as_invoice_user.current_user_as_follower', 'False')
         res.update({
             'use_current_user': use_current_user == 'True',
+            'current_user_as_follower': current_user_as_follower == 'True',
         })
         if specific_user_id := ICP.get_param('current_user_as_invoice_user.specific_user_id'):
             res.update({
@@ -37,6 +46,7 @@ class ResConfigSettings(models.TransientModel):
         super(ResConfigSettings, self).set_values()
         ICP = self.env['ir.config_parameter'].sudo()
         ICP.set_param('current_user_as_invoice_user.use_current_user', str(self.use_current_user))
+        ICP.set_param('current_user_as_invoice_user.current_user_as_follower', str(self.current_user_as_follower))
         if self.specific_invoice_user_id:
             ICP.set_param('current_user_as_invoice_user.specific_user_id', str(self.specific_invoice_user_id.id))
 
--- replace_invoicing_user/wizard/sale_make_invoice_advance.py
+++ replace_invoicing_user/wizard/sale_make_invoice_advance.py
@@ -7,23 +7,21 @@ class SaleMakeInvoiceAdvance(models.TransientModel):
     def _prepare_invoice_values(self, order, so_line):
         invoice_vals = super()._prepare_invoice_values(order, so_line)
         
-        # Get configuration parameters
-        use_current_user = self.env['ir.config_parameter'].sudo().get_param(
+        # On utilise le contexte actuel qui doit déjà contenir les bonnes valeurs de _create_invoices
+        use_current_user = self.env.context.get('use_current_user', self.env['ir.config_parameter'].sudo().get_param(
             'current_user_as_invoice_user.use_current_user', 'True'
-        ).lower() == 'true'
-        
-        specific_user_id = int(self.env['ir.config_parameter'].sudo().get_param(
-            'current_user_as_invoice_user.specific_user_id', '0'
-        ))
+        ).lower() == 'true')
         
         # Determine which user to use
         if use_current_user:
-            user_id = self.env.user.id
+            # Utiliser l'utilisateur qui crée la facture
+            user_id = self.env.uid
         else:
-            user_id = specific_user_id if specific_user_id else self.env.user.id
-            # Ne pas tracker les followers si on n'utilise pas l'utilisateur courant
-            # Cette valeur sera lue depuis le contexte par le modèle account.move
-            self = self.with_context(no_follower_tracking=True, specific_invoice_user_id=specific_user_id)
+            # Utiliser l'utilisateur spécifique
+            specific_user_id = self.env.context.get('specific_invoice_user_id') or int(self.env['ir.config_parameter'].sudo().get_param(
+                'current_user_as_invoice_user.specific_user_id', '0'
+            ))
+            user_id = specific_user_id if specific_user_id else self.env.uid
             
         invoice_vals.update({
             'invoice_user_id': user_id,
@@ -32,15 +30,33 @@ class SaleMakeInvoiceAdvance(models.TransientModel):
         return invoice_vals
         
     def _create_invoices(self, sale_orders):
-        # Si on utilise un utilisateur spécifique, il faut aussi passer le contexte ici
+        # Gestion du contexte pour les followers et utilisateurs de facture
         use_current_user = self.env['ir.config_parameter'].sudo().get_param(
             'current_user_as_invoice_user.use_current_user', 'True'
         ).lower() == 'true'
         
+        # Vérifier si on doit ajouter l'utilisateur courant comme follower
+        current_user_as_follower = self.env['ir.config_parameter'].sudo().get_param(
+            'current_user_as_invoice_user.current_user_as_follower', 'False'
+        ).lower() == 'true'
+        
+        # Dans tous les cas, on utilise no_follower_tracking=True pour désactiver le comportement standard
+        # et appliquer notre propre logique dans account_move.py
+        ctx = {
+            'no_follower_tracking': True,
+            'use_current_user': use_current_user,
+            'current_user_as_follower': current_user_as_follower,
+            # Passer explicitement l'ID de l'utilisateur courant dans le contexte
+            # Cela permet de garantir que c'est bien l'utilisateur spécifié dans with_user() qui est utilisé
+            'explicit_user_id': self.env.user.id
+        }
+        
         if not use_current_user:
+            # Si on utilise un utilisateur spécifique, on l'ajoute au contexte
             specific_user_id = int(self.env['ir.config_parameter'].sudo().get_param(
                 'current_user_as_invoice_user.specific_user_id', '0'
             ))
-            self = self.with_context(no_follower_tracking=True, specific_invoice_user_id=specific_user_id)
-            
+            ctx['specific_invoice_user_id'] = specific_user_id
+        
+        self = self.with_context(**ctx)
         return super()._create_invoices(sale_orders)
