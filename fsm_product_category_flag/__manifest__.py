ca91d6873c0b6a32ef7f14a689aa38cf4087997b
Denis Durepos <ddurepos@durpro.com>
2025-12-02 21:04:42 -0500

    Added description to fsm_product_category_flag module manifest.

--- fsm_product_category_flag/__manifest__.py
+++ fsm_product_category_flag/__manifest__.py
@@ -7,6 +7,17 @@
     'website': 'https://www.bemade.org',
     'category': 'Product',
     'depends': ['product'],
+    'description': '''
+FSM Product Category Flag
+=========================
+
+This addon adds a simple **FSM Product** boolean on product categories.
+
+It is intended to be used by FSM-related reporting or business logic, such as
+the FSM Job Profitability module, to distinguish which product categories
+should be considered FSM service products when computing revenue or other
+metrics.
+''',
     'data': [
         'views/product_category_views.xml',
     ],
