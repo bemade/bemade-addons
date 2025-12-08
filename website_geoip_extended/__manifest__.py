814385f9d6b43ccffba8784f3d0808c7f18a3929
Marc Durepos <marc@bemade.org>
2025-12-08 15:19:28 -0500

    [19.0][MIG] website_geoip_extended

--- website_geoip_extended/__manifest__.py
+++ website_geoip_extended/__manifest__.py
@@ -1,9 +1,9 @@
 {
-    'name': "Bemade Custom Module",
-    'version': "18.0.0.1.0",
-    'summary': "Module personnalisé pour Bemade Inc",
-    'sequence': 10,
-    'description': """
+    "name": "Bemade Custom Module",
+    "version": "19.0.0.1.0",
+    "summary": "Module personnalisé pour Bemade Inc",
+    "sequence": 10,
+    "description": """
     Ce module personnalisé est développé pour étendre les fonctionnalités de base d'Odoo
 
     Fonctionnalités:
@@ -12,30 +12,30 @@
     - Obtenez les données de GeoIP et d'autres sources
     - Mise à jour du visiteur avec les nouvelles données
     """,
-    'category': 'Tools',
-    'author': "Benoit",
-    'website': "https://bemade.org",
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
-        'website',  # Ajoutez d'autres dépendances si nécessaire
+    "category": "Tools",
+    "author": "Benoit",
+    "website": "https://bemade.org",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "website",  # Ajoutez d'autres dépendances si nécessaire
     ],
-    'data': [
+    "data": [
         # Liste des fichiers de données XML ou CSV, par exemple:
         # 'views/templates.xml',
         # 'security/ir.model.access.csv',
-        'views/website_visitor_views.xml',
+        "views/website_visitor_views.xml",
     ],
-    'demo': [
+    "demo": [
         # Fichiers de données de démo, par exemple:
         # 'demo/demo_data.xml',
     ],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'external_dependencies': {
-        'python': [
-            'geoip2'
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "external_dependencies": {
+        "python": [
+            "geoip2"
         ],  # Ajoutez les dépendances Python ici, par exemple ['pandas']
     },
 }
