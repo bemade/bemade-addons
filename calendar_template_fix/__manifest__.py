f58727e78a0b9e8b73ef477fbea47253438f0b08
Marc Durepos <marc@bemade.org>
2026-01-27 09:23:38 -0500

    calendar_template_fix: fix calendar event templates with incorrect vidoe call url t-attr-href attributes (no string interpolation).

---
+++ calendar_template_fix/__manifest__.py
@@ -0,0 +1,22 @@
+#    Bemade Inc.
+#
+#    Copyright (C) 2024-today Bemade Inc. (<https://www.bemade.org>).
+#    Author: Marc Durepos (Contact : mdurepos@durpro.com)
+#
+#    This program is under the terms of the GNU Lesser General Public License (LGPL-3)
+#    For details, visit https://www.gnu.org/licenses/lgpl-3.0.en.html
+
+{
+    "name": "Calendar Template Fix",
+    "version": "19.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Productivity",
+    "summary": "Fixes video call link rendering in calendar email templates",
+    "author": "Bemade Inc.",
+    "website": "https://www.bemade.org",
+    "depends": ["calendar"],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
