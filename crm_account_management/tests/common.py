299d1dc28e2e4a7feee45ae1458a34683b6b452d
Marc Durepos <marc@bemade.org>
2026-04-23 11:07:34 -0400

    fix(crm_account_management): use group_ids (Odoo 19) instead of groups_id in test fixture (task #3181)

--- crm_account_management/tests/common.py
+++ crm_account_management/tests/common.py
@@ -10,7 +10,7 @@ class OUTestCommon(AccountTestInvoicingCommon):
     def setUpClass(cls):
         super().setUpClass()
         group_sale = cls.env.ref("sales_team.group_sale_salesman")
-        cls.env.user.groups_id = [Command.link(group_sale.id)]
+        cls.env.user.group_ids = [Command.link(group_sale.id)]
 
     def _make_order(self, vals):
         """Create a sale.order."""
