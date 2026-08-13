73cc8dde718b035e8b03dfac3365ff52aa75e41b
Marc Durepos <marc@bemade.org>
2026-08-13 15:41:44 -0400

    chore(crm_account_management): bump version to 19.0.1.0.2

--- crm_account_management/__manifest__.py
+++ crm_account_management/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "CRM Account Management",
-    "version": "19.0.1.0.1",
+    "version": "19.0.1.0.2",
     "category": "Sales/CRM",
     "summary": "Customer Account Management with Dashboard and Reporting",
     "description": """
